f18ed0f91d93a7f8f30dba86605f4d321d4fea87
Guillermo Tuyub Canul <guillermoetuyub@yahoo.com>
2019-05-07 14:55:37 -0500

    end of day 2

--- -project0_gt/Program.cs
+++ -project0_gt/Program.cs
@@ -7,14 +7,21 @@ namespace _project0_gt
         static string name = "";
         static void Main(string[] args)
         {
-            Employee emp = new Employee();
-            emp.firstName = "Carol";
-            emp.lastName = "Baxtor";
-            emp.ssn = "asdf32";
-            emp.age = 22;
-            emp.salary = 2000000;
+            Employee emp = new Employee(); //default constructor
+            //emp.firstName = "Carol";
+            //emp.lastName = "Baxtor";
+            //emp.ssn = "asdf32";
+            //emp.age = 22;
+            //emp.salary = 2000000;
+            //emp.human = false; cannot be done since it is changing a const variable
+            //emp.GetDetails();
 
-            emp.GetDetails();
+            // Calling parameterized Constructor with named parameters
+
+            //Employee emp2 = new Employee("palmer", "calgoris", "asdf", 27, 250.00M);
+
+            //Employee emp3 = new Employee(lastName:"calgoris", firstName: "Trini", ssn:"asdf234", age:29);
+            Console.Write($"The salary is {emp._Salary}");
         }
     }
 }
--- -project0_gt/Types.cs
+++ -project0_gt/Types.cs
@@ -4,14 +4,51 @@ namespace _project0_gt
 {
     class Employee // properties
     {
+
+        //internal const bool ishuman = true; //val cannot be changed
         //variables camelCase
+        //declaration of variables
+
         internal  string firstName, lastName, ssn;
         internal int age;
-        internal decimal salary;
+        private decimal salary;
 
+        //properties: Smart fields to access private variables outside the class
+        public decimal _Salary{
+            get{
+                return salary;
+            }
+            set{
+                salary = value;
+            }
+        }
         //methods = Specifications + definition(body)
         public void GetDetails(){
             Console.WriteLine($"Name {firstName} {lastName} has Social Security - {ssn}. The Salary is {salary}");
+        }
+
+        //constructors in C# : Special methods .. used to intialize memory to the object
+        //parameterless constructor
+        public Employee(){ //constructor should have name as the class
+            firstName = "Cameron";
+            lastName = "Coley";
+            ssn = "asdf32";
+            age = 22;
+            salary = 2000000.00M;
+            Console.WriteLine($"Name {firstName} {lastName} has Social Security - {ssn}. The Salary is {salary}");
+
+        }
+
+        //parameterized constructors
+        public Employee(string firstName,string lastName,string ssn, int age, decimal salary=5000.50M)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.ssn = ssn;
+            this.age = age;
+            this.salary = salary;
+            Console.WriteLine($"Name {firstName} {lastName} has Social Security - {ssn}. The Salary is {salary}");
+
         }
     }
 }
