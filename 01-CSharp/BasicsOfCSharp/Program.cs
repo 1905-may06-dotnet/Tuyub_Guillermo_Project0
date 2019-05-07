a1aae622ca473105f6f94261c250efc71d23dcc3
Guillermo Tuyub Canul <guillermoetuyub@yahoo.com>
2019-05-07 11:46:42 -0500

    second commit

---
+++ 01-CSharp/BasicsOfCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicsOfCSharp
+{
+    class Program
+    {
+       static string name;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter your name")
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
