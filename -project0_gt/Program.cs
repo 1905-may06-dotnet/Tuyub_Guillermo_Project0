d566e29ffdba78330007e777763034797dc74664
Guillermo Tuyub Canul <guillermoetuyub@yahoo.com>
2019-05-07 14:18:59 -0500

    data types, access modifiers

--- -project0_gt/Program.cs
+++ -project0_gt/Program.cs
@@ -5,12 +5,16 @@ namespace _project0_gt
     class Program// properties
     {
         static string name = "";
-    }
-        }
         static void Main(string[] args)
         {
             Employee emp = new Employee();
-            emp.firstName = "Carol"
+            emp.firstName = "Carol";
+            emp.lastName = "Baxtor";
+            emp.ssn = "asdf32";
+            emp.age = 22;
+            emp.salary = 2000000;
+
+            emp.GetDetails();
         }
     }
 }
--- -project0_gt/Types.cs
+++ -project0_gt/Types.cs
@@ -5,13 +5,13 @@ namespace _project0_gt
     class Employee // properties
     {
         //variables camelCase
-        string firstName, lastName, ssn;
-        int age;
-        decimal salary;
+        internal  string firstName, lastName, ssn;
+        internal int age;
+        internal decimal salary;
 
         //methods = Specifications + definition(body)
         public void GetDetails(){
-            Console.WriteLine("Name {0} {1} has Social Security - {2}. The Salary is {3}", firstName, lastName, ssn, salary);
+            Console.WriteLine($"Name {firstName} {lastName} has Social Security - {ssn}. The Salary is {salary}");
         }
     }
 }
